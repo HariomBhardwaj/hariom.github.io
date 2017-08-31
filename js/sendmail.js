06847bb6f9a1c493e9f4a84e5c4a634c68b7689a
Amar D. Sharma <adsharma@svam.com>
2017-08-31 11:08:45 +0530

    -

--- js/sendmail.js
+++ js/sendmail.js
@@ -1,22 +1,19 @@
 var myform = $("form#myform");
 myform.submit(function(event){
 	event.preventDefault();
- 
+
   // Change to your service ID, or keep using the default service
   var service_id = "default_service";
-  var template_id = "template_FLDVV1BB";
+  var template_id = "template_ea3Ju9x4";
 
   myform.find("button").text("Sending...");
   emailjs.sendForm(service_id,template_id,"myform")
   	.then(function(){ 
-    	alert("Query Sent! We will Get Back to You Soon.");
+    	alert("Sent!");
        myform.find("button").text("Send");
     }, function(err) {
-       alert("Send query failed!\r\n Response:\n " + JSON.stringify(err));
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
        myform.find("button").text("Send");
     });
   return false;
 });
-function formreset() {
-    document.getElementById("myform").reset();
-}
